fd4c61c1eb314ffc68799175ce1670315acbe400
chentao <chentao@elex.tech.com>
2019-09-17 16:35:16 +0800

    移动 & 行走动画 调试中

--- Assets/Scripts/HeroMoveController.cs
+++ Assets/Scripts/HeroMoveController.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class HeroMoveController : MonoBehaviour
 {
-    enum Direction
-    {
-        Left,
-        Right,
-        Up,
-        Down,
-    }
+    // =========== Public ===========
+    public Animator _animator;
+    public Rigidbody2D _rigidbody;
 
-    private bool m_isMoving = false;
-    private float m_cellSize = 32.0f;
-    // Start is called before the first frame update
+    // =========== Private ===========
+    private float _cellSize = 32.0f;
+    private float _duration = 0.5f;
+
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
+        float vertical = Mathf.Approximately(horizontal, 0.0f) ? Input.GetAxis("Vertical") : 0.0f;
+        bool isInputExist = !Mathf.Approximately(horizontal, 0.0f) || !Mathf.Approximately(vertical, 0.0f);
+        bool isMoving = _animator.GetBool("Moving");
 
-        //Vector2 posPre = transform.position;
-        //posPre.x += horizontal>0? 
-    }
-
-    void StepOnce(Direction direction)
-    {
-        if (!m_isMoving)
+        if (isInputExist)
         {
-            
+            if (isMoving)
+            {
+                // =========== Movement ===========
+                Vector2 move = new Vector2(horizontal, vertical);
+                Vector2 posPre = _rigidbody.position;
+                Vector2 posCur = posPre + move * _cellSize / _duration * Time.deltaTime;
+                _rigidbody.MovePosition(posCur);
+            } else
+            {
+                // =========== Animation ===========
+                _animator.SetFloat("MoveX", horizontal);
+                _animator.SetFloat("MoveY", vertical);
+                _animator.SetBool("Moving", true);
+            }
         }
     }
 }
